4c2f05cf00476e70494101c4f4ba85466b729679
natashabuckham <natashaisobel@gmail.com>
2023-05-31 16:04:25 +0100

    Set up local database

--- api/app.js
+++ api/app.js
@@ -2,11 +2,8 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
-const JWT = require("jsonwebtoken");
 
-const postsRouter = require("./routes/posts");
-const tokensRouter = require("./routes/tokens");
-const usersRouter = require("./routes/users");
+const MessagesRouter = require("./routes/messages");
 
 const app = express();
 
@@ -17,31 +14,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// middleware function to check for valid tokens
-const tokenChecker = (req, res, next) => {
-
-  let token;
-  const authHeader = req.get("Authorization")
-
-  if(authHeader) {
-    token = authHeader.slice(7)
-  }
-
-  JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
-    if(err) {
-      console.log(err)
-      res.status(401).json({message: "auth error"});
-    } else {
-      req.user_id = payload.user_id;
-      next();
-    }
-  });
-};
 
 // route setup
-app.use("/posts", tokenChecker, postsRouter);
-app.use("/tokens", tokensRouter);
-app.use("/users", usersRouter);
+app.use("/", MessagesRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
