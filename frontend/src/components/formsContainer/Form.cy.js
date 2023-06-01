f2d0c221a3df327dc4eb3728079076e044877792
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-06-01 14:19:30 +0100

    untested

--- frontend/src/components/formsContainer/Form.cy.js
+++ frontend/src/components/formsContainer/Form.cy.js
@@ -1,14 +1,25 @@
-// import FormsContainer from "./FormsContainer";
+import Form from "./Form"
 
-// describe('FormsContainer', () => {
-//   beforeEach(() => {
-//     cy.visit('/'); 
-//   });
+describe("Form", () => {
+  it("calls the / endpoint", () => {
+    let genres;
 
-//   it('should select a character', () => {
-//     const selectedCharacter = 'Mickey Mouse';
+    cy.mount(
+      <Form dropdownItems={genres} selectionField="Genre" />
+    )
 
-//     cy.get('#character').select(selectedCharacter);
-//     cy.get('p').should('have.text', `Selected: ${selectedCharacter}`);
-//   });
-// });
+    cy.get("#comment").type("this is a test message");
+    cy.get("#submit-comment").click();
+    cy.wait('@createCommentRequest').then( interception => {
+      expect(interception.response.body.message).to.eq("OK");
+      expect(interception.request.body.comment).to.eq("this is a test message");
+    })
+  })
+})
+
+
+
+
+// label is correct: field is Genre when field is clicked
+// right field is appearing when rendering the component
+// clicking dropdown shows the right things appearing
