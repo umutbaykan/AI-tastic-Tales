cdf39d57a07c296556b53e92044e24534fa73aac
natashabuckham <natashaisobel@gmail.com>
2023-06-01 13:55:19 +0100

    Add background colour to LoadingIcon component

--- frontend/src/components/loading/LoadingIcon.js
+++ frontend/src/components/loading/LoadingIcon.js
@@ -2,9 +2,11 @@ import './LoadingIcon.css';
 
 const LoadingIcon = () => {
   return (
-    <div className="container">
-      <h2>Crafting your story...</h2>
-      <img className='loading-icon' src="/images/slow-turning-pages.gif" alt="book-gif" />
+    <div className="background">
+      <div className="container">
+        <h2>Crafting your story...</h2>
+        <img className='loading-icon' src="/images/slow-turning-pages.gif" alt="book-gif" />
+      </div>
     </div>
   )
 }
