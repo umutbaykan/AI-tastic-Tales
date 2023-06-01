6a372b428473244d2c6825514a3844971ab1d182
umut <umut_baykan@hotmail.com>
2023-06-01 13:26:05 +0100

    test coverage added on npm scripts, removed unnecessary status code generation

--- api/controllers/images.js
+++ api/controllers/images.js
@@ -6,8 +6,7 @@ const ImagesController = {
       const result = await client.generateImage(req.body.positivePrompt, req.body.negativePrompt);
       res.status(200).json({ imgUrl: result });
     } catch (error) {
-      const statusCode = error.status ? error.status : 500;
-      res.status(statusCode).json({ message: error.message });
+      res.status(error.status).json({ message: error.message });
     }
   },
 };
