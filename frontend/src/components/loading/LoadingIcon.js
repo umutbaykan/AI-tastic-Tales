ab634a727a2243a2667a4f56958516535e9b9373
natashabuckham <natashaisobel@gmail.com>
2023-06-01 13:15:05 +0100

    Implement basic loading gif component with test

---
+++ frontend/src/components/loading/LoadingIcon.js
@@ -0,0 +1,11 @@
+import './LoadingIcon.css';
+
+const LoadingIcon = () => {
+  return (
+    <div>
+      <img className='loading-icon' src="./public/slow-turning-pages.gif" alt="book-gif" />
+    </div>
+  )
+}
+
+export default LoadingIcon;
