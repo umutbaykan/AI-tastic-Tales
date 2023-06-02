b867cc9cd842ac686afbf3fbc64514944ce87d25
umut <umut_baykan@hotmail.com>
2023-06-02 15:33:55 +0100

    hardcoded image to test useEffect

--- frontend/src/components/result-page/ResultPage.js
+++ frontend/src/components/result-page/ResultPage.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from '../image/image';
 import Story from '../story/Story';
 
 const ResultPage = ({ navigate }) => {
   
   const [userChoices, setUserChoices] = useState(window.localStorage.getItem("userChoices"));
+  const [imgUrl, setImgUrl] = useState()
 
-    // GPTClientCall(formValues)
-    // sdClientCall(formValues)
+  useEffect(() => {
+    // GPTClientCall(userChoices)
+    sdClientCall(userChoices)
+  }, [])
+  
 
-  const sdClientCall = (userInput) => {
+  const sdClientCall = (userChoices) => {
     fetch('/images', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(userInput)
+      body: userChoices
     })
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => setImgUrl('https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80'))
   }
 
-  const GPTClientCall = (userInput) => {
+  const GPTClientCall = (userChoices) => {
     fetch("/story", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(userInput)
+      body: userChoices
     })
     .then(response => response.json())
     .then(data => console.log(data))
@@ -37,7 +41,7 @@ const ResultPage = ({ navigate }) => {
     <div className="result-page">
       <h1>Here's your story!</h1>
       <div className="container">
-        <Image />
+        <Image link={imgUrl}/>
         <Story />
         <div className="buttons">
           <button>Save this story</button>
