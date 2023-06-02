aeddef1fde357230309ab725e81cbacdd82395b2
umut <umut_baykan@hotmail.com>
2023-06-02 16:56:36 +0100

    removal of database and references

--- api/models/message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const MessageSchema = new mongoose.Schema({
-  message: String
-});
-
-const Message = mongoose.model("Message", MessageSchema);
-
-module.exports = Message;
