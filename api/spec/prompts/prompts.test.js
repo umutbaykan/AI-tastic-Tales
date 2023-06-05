632f34695d185e1f889655283536d8def7331478
djohn06 <danielajohn1406@gmail.com>
2023-06-05 15:44:18 +0100

    Previous test altered and new test | Both passing

--- api/spec/prompts/prompts.test.js
+++ api/spec/prompts/prompts.test.js
@@ -1,15 +1,26 @@
 const { matchSelectionCriteria } = require('../../prompts/prompts');
 
 describe('matchSelectionCriteria', () => {
-    const userSelection = { genre: 'dystopia', style: 'cartoon' };
+  it('displays Darth Vader under specific criteria', () => {
+    const userSelection = {character:'Darth Vader', genre: 'Dystopian', style: 'Cartoon' };
+    const expectedCriteria = {
+      positivePrompts: "{{Darth Vader}}, human body, male, all black, fine detail, sharp focus, {{dystopian}}, oppressive, society, resistance, dark, sci-fi, synthwave neon retro, dramatic lighting, synthwave neon retro, {{cartoon}}, funny, whimsical, colorful, lively, vivid, digital art, smooth, sharp focus, 4k, highly detailed, smooth drawn",
+      negativePrompts: "disfigured, deformed, mutated hands, disproportioned, bad proportions, cross-eyed, low quality, blurry, female, utopia, harmony, peace, ideal, blurry, dark, grim, sinister, serious, realistic, photorealistic, unreal engine"
+    };
+
+    const result = matchSelectionCriteria(userSelection);
+    expect(result).toEqual(expectedCriteria);
+  });
+
   
-    it('returns the expected criteria for genre and style', () => {
-      const expectedCriteria = {
-        negativePrompts: "utopia, harmony, peace, ideal, happy, dark, grim, sinister, serious, realistic, photorealistic, unreal engine",
-        positivePrompts: "oppressive, society, resistance, dystopian, dark, photorealistic dark sci-fi concept art, trending on artstation, synthwave neon retro, realistic proportions, dramatic lighting, synthwave neon retro, funny, whimsical, colorful, lively, cartoon, vivid, digital art, smooth, sharp focus, 4k, highly detailed, smooth drawn"
-      };
-  
-      const result = matchSelectionCriteria(userSelection);
-      expect(result).toEqual(expectedCriteria);
-    });
+  it('displays Hermione Granger under specific criteria', () => {
+    const userSelection = {character:'Hermione Granger', genre: 'Cyberpunk', style: 'Anime' };
+    const expectedCriteria = {
+      positivePrompts: "{{Hermione Granger}}, human body, female, Hogwarts, wearing Hogwarts uniform, fine detail, sharp focus, {{cyberpunk}}, steampunk, colourful neon lights, night cityscape, purple neon, skyscrapers with neon lights, highly detailed, futuristic, {{anime}}, anime drawing, anime background, Makoto Shinkai, beautiful, highly detailed, high quality",
+      negativePrompts: "disfigured, deformed, mutated hands, disproportioned, bad proportions, cross-eyed, low quality, blurry, male, sunny daytime, suburbs, fantasy, magical, blurry, realistic, 3D, watercolour, low quality:1.4"
+    };
+
+    const result = matchSelectionCriteria(userSelection);
+    expect(result).toEqual(expectedCriteria);
+  });
 });
