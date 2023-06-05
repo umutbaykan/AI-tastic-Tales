89fb4983ae76957b06941767192282028a100511
djohn06 <danielajohn1406@gmail.com>
2023-06-05 15:43:46 +0100

    New prompts created for each selection

--- api/prompts/prompts.js
+++ api/prompts/prompts.js
@@ -1,48 +1,74 @@
 const sdPromptBank = {
     character: {
-        spiderman: {
+        'Spiderman': {
             positivePrompts: ['{{Spiderman in superhero suit}}', 'human body', 'male', 'fine detail', 'red and blue', 'sharp focus'],
             negativePrompts: ['disfigured', 'deformed', 'mutated hands', 'disproportioned', 'bad proportions', 'cross-eyed', 'low quality', 'blurry', 'female']
         },
-        rapunzel: {
+        'Rapunzel': {
             positivePrompts: ['{{Rapunzel}}', 'human body', 'female', 'very long blonde hair', 'fine detail', 'sharp focus'],
             negativePrompts: ['disfigured', 'deformed', 'mutated hands', 'disproportioned', 'bad proportions', 'cross-eyed', 'low quality', 'blurry', 'male']
         },
-        darth_vader: {
+        'Darth Vader': {
             positivePrompts: ['{{Darth Vader}}', 'human body', 'male', 'all black', 'fine detail', 'sharp focus'],
             negativePrompts: ['disfigured', 'deformed', 'mutated hands', 'disproportioned', 'bad proportions', 'cross-eyed', 'low quality', 'blurry', 'female']
         },
-        wonder_woman: {
+        'Wonder Woman': {
             positivePrompts: ['{{Wonder Woman in superhero suit}}', 'human body', 'female', 'black hair', 'fine detail', 'sharp focus'],
             negativePrompts: ['disfigured', 'deformed', 'mutated hands', 'disproportioned', 'bad proportions', 'cross-eyed', 'low quality', 'blurry', 'male']
         },
-        hermione_granger: {
-            positivePrompts: ['{{Hermione Granger}}', 'human body', 'female', 'Hogwarts', 'wearing hogwarts uniform', 'fine detail', 'sharp focus'],
+        'Hermione Granger': {
+            positivePrompts: ['{{Hermione Granger}}', 'human body', 'female', 'Hogwarts', 'wearing Hogwarts uniform', 'fine detail', 'sharp focus'],
             negativePrompts: ['disfigured', 'deformed', 'mutated hands', 'disproportioned', 'bad proportions', 'cross-eyed', 'low quality', 'blurry', 'male']
         },
-        batman: {
+        'Batman': {
             positivePrompts: ['{{Batman in batsuit}}', 'human body', 'male', 'fine detail', 'sharp focus'],
             negativePrompts: ['disfigured', 'deformed', 'mutated hands', 'disproportioned', 'bad proportions', 'cross-eyed', 'low quality', 'blurry', 'female']
         }
     },
+
     genre: {
-        dystopia: {
-            positivePrompts: ['oppressive', 'society', 'resistance', 'dystopian', 'dark', 'photorealistic dark sci-fi concept art', 'trending on artstation, synthwave neon retro', 'realistic proportions', 'dramatic lighting', 'synthwave neon retro'],
-            negativePrompts: ['utopia', 'harmony', 'peace', 'ideal', 'happy'],
+        'Dystopian': {
+            positivePrompts: ['{{dystopian}}', 'oppressive', 'society', 'resistance', 'dark', 'sci-fi', 'synthwave neon retro', 'dramatic lighting', 'synthwave neon retro'],
+            negativePrompts: ['utopia', 'harmony', 'peace', 'ideal', 'blurry'],
         },
-        fairytale: {
-            positivePrompts: ['magic', 'enchanted', 'happily ever after', 'fantasy', 'adventure', 'highly detailed', 'intricate', 'elegant', 'clouds', 'vivid colours'],
-            negativePrompts: ['dark', 'grim', 'tragedy', 'horror', 'sorrow', 'dystopian', 'sci-fi', 'space', 'future']
+        'Fairytale': {
+            positivePrompts: ['{{fairytale}}', 'magic', 'enchanted', 'happily ever after', 'fantasy', 'adventure', 'highly detailed', 'intricate', 'elegant', 'clouds', 'vivid colours'],
+            negativePrompts: ['dystopian', 'sci-fi', 'space', 'future', 'sorrow', 'blurry']
         },
+        'Western': {
+            positivePrompts: ['{{western}}', 'wild west', 'mountains', 'desert', 'sunset lighting', 'horses running in the back', 'warm colours', 'cowboys'],
+            negativePrompts: ['fantasy', 'magical', 'grass fields', 'blurry']
+        },
+        'Cyberpunk': {
+            positivePrompts: ['{{cyberpunk}}', 'steampunk', 'colourful neon lights', 'night cityscape', 'purple neon', 'skyscrapers with neon lights', 'highly detailed', 'futuristic'],
+            negativePrompts: ['sunny daytime', 'suburbs', 'fantasy', 'magical', 'blurry']
+        },
+        'Sci-Fi': {
+            positivePrompts: ['{{sci-fi}}', 'futuristic', 'space', 'spaceships', 'planets and moons', 'Jim Burns', 'new worlds', 'vivid colours', 'futuristic clothing'],
+            negativePrompts: ['fantasy', 'magical', 'western']
+        }
+
     },
     style: {
-        cartoon: {
-            positivePrompts: ['funny', 'whimsical', 'colorful', 'lively', 'cartoon', 'vivid', 'digital art', 'smooth', 'sharp focus', '4k', 'highly detailed', 'smooth drawn'],
+        'Cartoon': {
+            positivePrompts: ['{{cartoon}}', 'funny', 'whimsical', 'colorful', 'lively', 'vivid', 'digital art', 'smooth', 'sharp focus', '4k', 'highly detailed', 'smooth drawn'],
             negativePrompts: ['dark', 'grim', 'sinister', 'serious', 'realistic', 'photorealistic', 'unreal engine']
         },
-        realistic: {
-            positivePrompts: ['vibrant', 'detailed', 'realistic', 'high-resolution', 'photorealistic', '8k', 'masterpiece', 'detailed', 'sharp focus', 'highly detailed'],
+        'Realistic': {
+            positivePrompts: ['{{realistic}}', 'vibrant', 'detailed', 'high-resolution', 'photorealistic', '8k', 'masterpiece', 'highly detailed', 'sharp focus', 'canon m50'],
             negativePrompts: ['cartoonish', 'abstract', 'surreal']
+        },
+        'Pixar': {
+            positivePrompts: ['{{pixar graphics}}', 'disney graphics', 'unreal engine 5', '4k', 'highly detailed', 'high quality', 'surreal'],
+            negativePrompts: ['realistic', 'painting', 'dark']
+        },
+        'Anime': {
+            positivePrompts: ['{{anime}}', 'anime drawing', 'anime background', 'Makoto Shinkai', 'beautiful', 'highly detailed', 'high quality'],
+            negativePrompts: ['realistic', '3D', 'watercolour', 'low quality:1.4']
+        },
+        'Painting': {
+            positivePrompts: ['{{painting}}', 'handdrawn painting', 'paint brushstrokes', 'Jacques-Laurent Agasse', 'highly detailed', 'high quality'],
+            negativePrompts: ['realistic', '3d', 'low quality:1.4', 'unreal engine', 'canon m50']
         },
     },
 }
