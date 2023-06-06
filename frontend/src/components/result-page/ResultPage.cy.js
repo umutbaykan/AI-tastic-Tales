6ff4ebef6e7d53f78ba7f0b54134d43ec7a416fe
natashabuckham <natashaisobel@gmail.com>
2023-06-06 17:58:43 +0100

    Merge main into branch

--- frontend/src/components/result-page/ResultPage.cy.js
+++ frontend/src/components/result-page/ResultPage.cy.js
@@ -3,12 +3,18 @@ import ResultPage from "./ResultPage";
 
 describe("ResultPage", () => {
   beforeEach(() => {
+    cy.clearLocalStorage();
     cy.intercept("POST", "/images", { fixture: "images.json" }).as(
       "imagesRequest"
+      // mock response from the server - message history
     );
     cy.intercept("POST", "/story", { fixture: "story.json" }).as(
       "storyRequest"
     );
+  });
+
+  afterEach(() => {
+    cy.clearLocalStorage();
   });
 
   it("shows the result page when loaded", () => {
@@ -23,6 +29,16 @@ describe("ResultPage", () => {
       cy.get(".buttons").should("contain", "What happens next?");
       cy.get(".buttons").should("contain", "Steer this story");
       cy.get(".buttons").should("contain", "Refresh the story");
+    });
+  });
+
+  it("redirects to '/' when the home button is clicked", () => {
+    mount(<ResultPage navigate={() => {}} />);
+
+    cy.wait(["@imagesRequest", "@storyRequest"]).then(() => {
+      cy.get(".results-page-home").click();
+      cy.url().should("eq", "http://localhost:3000/");
+      cy.getAllLocalStorage('userChoices').should('be.null');
     });
   });
 });
