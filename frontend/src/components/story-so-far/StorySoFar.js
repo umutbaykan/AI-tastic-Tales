f79d2646db18dfa8f36fdd6372acaf0f1abe9dd8
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-06-07 14:00:46 +0100

    download to pdf added

--- frontend/src/components/story-so-far/StorySoFar.js
+++ frontend/src/components/story-so-far/StorySoFar.js
@@ -3,6 +3,7 @@ import Image from "../image/image";
 import Story from "../story/Story";
 import HomeButton from "../home-button/HomeButton";
 import "./StorySoFar.css";
+import html2pdf from "html2pdf.js";
 
 const StorySoFar = ({ navigate  }) => {
   const [userChoices, setUserChoices] = useState(
@@ -12,10 +13,19 @@ const StorySoFar = ({ navigate  }) => {
   const messageHistory = userChoices.messageHistory;
   const imageHistory = userChoices.imageHistory;
 
+  const handleDownloadPDF = () => {
+    const element = document.querySelector('.story-so-far');
+    html2pdf()
+      .set({ margin: 0.5, filename: 'story_so_far.pdf' })
+      .from(element)
+      .save();
+  };
+
   return (
     <>
       <HomeButton navigate={ navigate }/>
       <h1 className="title">Your story so far!</h1>
+      <button className="download-button" onClick={handleDownloadPDF}>Download PDF</button>
         <div className="story-so-far">
         {messageHistory.map((content, index) => (
           <div key={index}>
