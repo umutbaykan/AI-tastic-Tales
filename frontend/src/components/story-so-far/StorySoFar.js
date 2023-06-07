bc1726bb9fd67aedb08af9ef4437da30fef83cf7
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-06-07 11:46:18 +0100

    Added route on app.js for StorySoFar

--- frontend/src/components/story-so-far/StorySoFar.js
+++ frontend/src/components/story-so-far/StorySoFar.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Image from "../image/image";
 import Story from "../story/Story";
+import HomeButton from "../home-button/HomeButton";
 
-const StorySoFar = () => {
+const StorySoFar = ({ navigate  }) => {
   const [messageHistory, setMessageHistory] = useState(
     JSON.parse(window.localStorage.getItem("messageHistory")) || []
   );
@@ -12,15 +13,18 @@ const StorySoFar = () => {
   );
 
   return (
-    <div className="story-so-far">
-      <h1>Your story so far!</h1>
-      {messageHistory.map((content, index) => (
-        <div key={index}>
-          <Image link={imageHistory[index]} />
-          <Story content={content} />
+    <>
+      <HomeButton navigate={ navigate }/>
+        <div className="story-so-far">
+        <h1>Your story so far!</h1>
+        {messageHistory.map((content, index) => (
+          <div key={index}>
+            <Image link={imageHistory[index]} />
+            <Story content={content} />
+          </div>
+        ))}
         </div>
-      ))}
-    </div>
+    </>
   );
 };
 
